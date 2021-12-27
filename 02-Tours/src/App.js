47e515859e551e92e61baa0212860f8423918f95
charit <charit@gmail.com>
2021-12-27 02:09:35 -0500

    Tour data is fetched from remote API

--- 02-Tours/src/App.js
+++ 02-Tours/src/App.js
@@ -4,8 +4,37 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try{
+      setLoading(true);
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      console.log(tours);
+    }catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return <h2>Tours Project Setup</h2>
-}
+
+};
+  
 
 export default App
